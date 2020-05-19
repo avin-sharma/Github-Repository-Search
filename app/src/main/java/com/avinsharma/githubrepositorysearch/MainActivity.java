dffb0000440b22d6a6180481a2a57b595db6430f
Avin Sharma <sharmaavin@hotmail.com>
2020-05-19 18:34:21 -0400

    Initial setup completed.

--- app/src/main/java/com/avinsharma/githubrepositorysearch/MainActivity.java
+++ app/src/main/java/com/avinsharma/githubrepositorysearch/MainActivity.java
@@ -3,12 +3,23 @@ package com.avinsharma.githubrepositorysearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchQueryEditText;
+    private Button mSearchButton;
+    private TextView mSearchResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchQueryEditText = findViewById(R.id.et_search_bar);
+        mSearchButton = findViewById(R.id.button_search);
+        mSearchResultTextView = findViewById(R.id.tv_search_result);
     }
 }
