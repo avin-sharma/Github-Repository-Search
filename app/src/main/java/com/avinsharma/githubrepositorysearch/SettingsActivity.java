960d684ae56aef95fdfc136102888aff0e6b66d7
Avin Sharma <sharmaavin@hotmail.com>
2020-06-21 14:00:40 -0400

    Fix Fixed data loss in navigation between MainActivity and SettingsActivity

--- app/src/main/java/com/avinsharma/githubrepositorysearch/SettingsActivity.java
+++ app/src/main/java/com/avinsharma/githubrepositorysearch/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.avinsharma.githubrepositorysearch;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
